9fcf205d1f9038bff257ac3f69d78a9ee5aec68b
Daniel-Fermoselle <daniel.fermoselle@gmail.com>
2020-01-04 13:53:48 +0000

    First commit from the Atom original repository
    It is yet needed to update the readme file with all the setup info. Also
    add informations about what is this and how does it work.

---
+++ spec/script-it-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ScriptItView from '../lib/script-it-view';
+
+describe('ScriptItView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
